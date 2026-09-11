211f68883b260b8e7f02ff9a4c525e566a8a4c05
23L: Šalát Jakub <23LSalatJ@student.panska.cz>
2026-09-11 12:12:40 +0200

    2026-09-11/4

---
+++ 2026-09-11_kalk_smenarna.py
@@ -0,0 +1,8 @@
+import requests
+from_c = input("Z meny: ")
+to_c = input("Do meny: ")
+base = float(input(f"Prevest kolik z {from_c} do {to_c}: "))
+link = f"https://api.frankfurter.dev/v2/rates?base={from_c.upper()}&quotes={to_c.upper()}"
+x = requests.get(link)
+rate = float(str(x.content).split(",")[-1].split(":")[-1].split("}")[0])
+print(f"{base} {from_c} = {rate*base} {to_c}")
