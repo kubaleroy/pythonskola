6c13cf621f93e917de96167b9d8b4226ae804b3d
23L: Šalát Jakub <23LSalatJ@student.panska.cz>
2026-09-11 11:37:22 +0200

    2026-09-11/1

---
+++ 2026-09-11_uloha1_real.py
@@ -0,0 +1,14 @@
+"""
+Analýza
+
+Vstupy: int sekund kladny cislo vetsi nez 0
+
+Výstupy: vypište, kolik je to hodin, minut a sekund, ve tvaru „H h M min S s“
+
+Omezení/okrajové případy: 0, desetina mista, ..
+
+Návrh
+
+Kroky: input-> prepsat typ -> vzit pocet hodin a zbytek -> ze zbytku vzit pocet minut a 
+"""
+print(str(float("3665")).split("."))
