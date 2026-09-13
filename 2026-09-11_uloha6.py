b569ae3c82fc7441a14d328c1b973e5699f17db5
kubaleroy <salatjakub7@gmail.com>
2026-09-13 15:21:27 +0200

    2026-09-13

--- 2026-09-11_uloha6.py
+++ 2026-09-11_uloha6.py
@@ -1,3 +1,6 @@
-n = input("Kolik cisel: ")
+n = input("Kolik cisel: ") 
+if float(n) <= 0 or float(n) != int(float(n)):
+    print("Neplatny pocet cisel")
+    exit()
 l = [float(input(f"Cislo {x+1}: ")) for x in range(int(n))]
 print(f"součet= {sum(l):.2f}, průměr= {sum(l)/int(n):.2f}, maximum= {max(l):.2f} a minimum= {min(l):.2f}")
