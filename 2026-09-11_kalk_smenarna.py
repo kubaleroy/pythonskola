51f8daf75f05016f824265316219952cf96ed16f
kubaleroy <salatjakub7@gmail.com>
2026-09-13 15:23:16 +0200

    2026-09-13/1

--- 2026-09-11_kalk_smenarna.py
+++ 2026-09-11_kalk_smenarna.py
@@ -4,5 +4,9 @@ to_c = input("Do meny: ")
 base = float(input(f"Prevest kolik z {from_c} do {to_c}: "))
 link = f"https://api.frankfurter.dev/v2/rates?base={from_c.upper()}&quotes={to_c.upper()}"
 x = requests.get(link)
-rate = float(str(x.content).split(",")[-1].split(":")[-1].split("}")[0])
+try:
+    rate = float(str(x.content).split(",")[-1].split(":")[-1].split("}")[0])
+except ValueError:
+    print("Neplatne kody meny")
+    exit()
 print(f"{base} {from_c} = {rate*base:.2f} {to_c}")
